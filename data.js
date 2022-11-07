d348b5676516188c3657121ed8dc9bda51450d89
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2022-11-07 21:28:06 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    mongoLink:'mongodb://kavya9109:kavya9109@ac-f7sytui-shard-00-00.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-01.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-02.vajav6l.mongodb.net:27017/?ssl=true&replicaSet=atlas-5z6f25-shard-0&authSource=admin&retryWrites=true&w=majority', //database link
+    
+    bot_token:'5602102162:AAHuL5pmm-rr_vOizUyZHA_TwHeV4b6Nubo',//bot token
+    
+    bot_name:'@Iron_TitaniumR1_Bot',//bot username with @
+    
+    bot_admin:5556540070,// admin telegram id
+    
+    channelsList : 
+    
+    ['@AirdropLauncher9109','@AIRDROPS_ALERTERS','@Meta_grow_team','@Technical_rishav_1','@devils_tech','@Thunder_Drop','@earningworld_in'],// channels list
+    
+    reffer_bonus:20000,// refferal bonus amount
+    
+    min_wd:60000,//min withdrawal amount
+    
+    daily_bonus:5000,// daily bonus amount
+    
+    currency:'IRON', //bot currency
+    
+    contractaddress :  '0xaAa5B9e6c589642f98a1cDA99B9D024B8407285A', //contract address of coin
+    
+    privateKey : 'f52d9fb7f04e64d93d825b8798c5ec897060efc3ec158187bc159131a8a6d1d4', //private key of wallet
+    
+    address : '0xEdA7bb3AcE6f8A70FaeB0b69D4Bfb9098b8ad6d9',//polygon address of wallet
+    //
+    paymentchannel: ['@Devils_Tech'] ,// payment channel username
+    
+    }
